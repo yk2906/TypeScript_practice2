7b0bcf732016a20186d6c82621948d1cb8be02e0
yk2906 <56224546+yk2906@users.noreply.github.com>
2021-11-07 20:51:53 +0900

    test

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,8 +1,7 @@
+import { VFC } from "react";
 import { TodoType } from "./types/todo";
 
-export const Todo = (
-  props: Pick<TodoType, "userId" | "title" | "completed">
-) => {
+export const Todo: VFC<Omit<TodoType, "id">> = (props) => {
   const { title, userId, completed = false } = props;
   const completedMark = completed ? "[完]" : "[未]";
 
