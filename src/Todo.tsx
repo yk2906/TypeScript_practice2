ea7d2056285bec13029d98966f6001ab48aee452
yk2906 <56224546+yk2906@users.noreply.github.com>
2021-11-07 17:26:31 +0900

    test

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,4 +1,4 @@
 export const Todo = (props) => {
-  const { title, userId } = props;
-  return <p>{`${title}(ユーザー:${userId})`}</p>;
+  const { title, userid } = props;
+  return <p>{`${title}(ユーザー:${userid})`}</p>;
 };
